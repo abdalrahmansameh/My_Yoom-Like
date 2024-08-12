974ac52615ba1bf6f89a7fbd8f325a9186296fc6
Abdulrahman.Sameh <darkness.itec.eg@gmail.com>
2024-08-12 13:08:08 +0200

    Update page.tsx

--- app/(auth)/sign-in/[[...sigin-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sigin-in]]/page.tsx
@@ -1,9 +1,1 @@
-import { SignIn } from '@clerk/nextjs';
 
-export default function SiginInPage() {
-  return (
-    <main className="flex h-screen w-full items-center justify-center">
-      <SignIn />
-    </main>
-  );
-}
