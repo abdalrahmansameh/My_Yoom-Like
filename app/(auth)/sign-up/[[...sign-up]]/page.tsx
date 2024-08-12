cc8e9de2f315ff7020c3862b44f7eaac2b93176d
Abdulrahman.Sameh <darkness.itec.eg@gmail.com>
2024-08-12 13:09:04 +0200

    Update page.tsx

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,1 @@
-import { SignUp } from '@clerk/nextjs';
 
-export default function SignUpPage() {
-  return (
-    <main className="flex h-screen w-full items-center justify-center">
-      <SignUp />
-    </main>
-  );
-}
